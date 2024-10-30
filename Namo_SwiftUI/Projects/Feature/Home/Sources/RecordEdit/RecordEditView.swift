3c0889e36b7de334cc2d0ec2e3263a5f66f6978c
FpRaArNkK <mspark1184@naver.com>
2024-10-30 17:02:40 +0900

    Feat: RecordEditView 추가

---
+++ Namo_SwiftUI/Projects/Feature/Home/Sources/RecordEdit/RecordEditView.swift
@@ -0,0 +1,15 @@
+//
+//  RecordEditView.swift
+//  FeatureHome
+//
+//  Created by 박민서 on 10/30/24.
+//
+
+import SwiftUI
+import SharedDesignSystem
+
+struct RecordEditView: View {
+    var body: some View {
+        Text("RecordEditView")
+    }
+}
