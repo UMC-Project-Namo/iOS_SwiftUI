81566ce37c2b05a03b57977e749b762645526cbe
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2024-12-11 23:06:58 +0900

    Feat: Photos 관련 기능 Utils로 분리

--- Namo_SwiftUI/Projects/Feature/Home/Sources/RecordEdit/ImageView/HomeEntireImageStore.swift
+++ Namo_SwiftUI/Projects/Feature/Home/Sources/RecordEdit/ImageView/HomeEntireImageStore.swift
@@ -5,35 +5,9 @@
 //  Created by 박민서 on 12/11/24.
 //
 
-import Foundation
 import ComposableArchitecture
-import SharedUtil
-
-import Photos
 import UIKit
-
-enum PhotoLibraryError: Error {
-    case unauthorized
-    case unknown(Error?)
-}
-
-func saveImageToPhotoLibrary(_ image: UIImage) async throws {
-    let status = await PHPhotoLibrary.requestAuthorization(for: .addOnly)
-    guard status == .authorized else { throw PhotoLibraryError.unauthorized }
-
-    try await withCheckedThrowingContinuation { continuation in
-        PHPhotoLibrary.shared().performChanges({
-            PHAssetChangeRequest.creationRequestForAsset(from: image)
-        }) { success, error in
-            if success {
-                continuation.resume()
-            } else {
-                continuation.resume(throwing: PhotoLibraryError.unknown(error))
-            }
-        }
-    }
-}
-
+import SharedUtil
 
 @Reducer
 public struct HomeEntireImageStore {
